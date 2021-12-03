54f078ec90dda18b915bd2927ba1897dc59c1e72
GigaMick <michael@songbox.rocks>
2021-12-03 09:58:27 +0000

    hosted fonts locally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,10 +32,6 @@
     <!-- Scripts -->
     <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
