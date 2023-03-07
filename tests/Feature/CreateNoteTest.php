be2e6bd5aabb61f7bbb56541065db1a76ada4a45
Aju Chacko <ajuchacko91@gmail.com>
2023-03-07 12:32:25 +0530

    Adds create note testsuite

---
+++ tests/Feature/CreateNoteTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Crypt;
+use App\Models\Note;
+use Tests\TestCase;
+use Mockery;
+
+class CreateNoteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    function user_can_create_an_encrypted_with_password_note() {
+        $message = 'Secret Message!';
+        $defaultExpiry = 7;
+
+        $response = $this->post('/create-note', [
+            'note' => $message,
+            'password' => 'secret'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertViewIs('note-summary');
+        $this->assertNotNull($note = Note::first());
+        $this->assertNotNull($note->token);
+        $this->assertNotNull($note->password);
+        $this->assertEquals(now()->addDays($defaultExpiry), $note->expiry_date);
+        $this->assertEquals($message, Crypt::decryptString($note->note));
+        $response->assertViewHas('note', function (Note $viewNote) use ($note) {
+            return $note->id === $viewNote->id;
+        });
+    }
+}
