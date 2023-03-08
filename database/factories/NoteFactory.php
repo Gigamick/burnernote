1069a43caaca6c4e8e615edf74b3f5241b9515f5
Aju Chacko <ajuchacko91@gmail.com>
2023-03-08 16:57:36 +0530

    Create NoteFactory.php

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+class NoteFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'note'          => encrypt('secret message'),
+            'password'      => Hash::make('secret'),
+            'expiry_date'   => now()->addDays(7),
+            'token'         => 'token',
+        ];
+    }
+}
