7f671e9d741dbb0b4b99778dcd6f83dced7f0776
Gigamick <michael@songbox.rocks>
2024-09-10 15:27:54 +0100

    new footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,8 @@
                         based notes that are completely erased from existence once they've been read.</h5>
                     <div class="row">
                         <div class="col-12">
-                            <p class="mt-0 text-muted">Utilising AES-256-CBC encryption signed with a message authentication
+                            <p class="mt-0 text-muted">Utilising AES-256-CBC encryption signed with a message
+                                authentication
                                 code (MAC).</p>
                         </div>
                     </div>
@@ -48,5 +49,38 @@
             </div>
         </div>
     </section>
+    <div class="fixed-bottom py-3 border-top">
+        <div class="d-flex justify-content-center align-items-center">
+            <style>
+                .pizzabtn {
+                    background: #333333;
+                    border-radius: 7px;
+                    font-size: 12px;
+                    color: white;
+                    padding: 0px 10px;
+                    font-weight: bold;
+                    height: 32px;
+                }
+
+                .pizzabtn:hover {
+                    text-decoration: none;
+                    color: white;
+                }
+            </style>
+            <div class="d-flex justify-content-center text-align-center">
+                <a href="https://buymeacoffee.com/gigamick" target="_blank"
+                   class="pizzabtn d-flex align-items-center">&#x1F355; Buy me a Pizza</a>
+            </div>
+            <div class="spacer mx-4">|</div>
+            {{--                        <a href="buymeacoffee.com/gigamick" class="text-muted">Sponsor on Github</a>--}}
+            <a href="https://github.com/sponsors/Gigamick" target="_blank"
+               class="pizzabtn d-flex align-items-center" style="gap: 8px">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-github" viewBox="0 0 16 16">
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+                </svg>
+                Sponsor on Github</a>
+        </div>
+    </div>
 
 @endsection
